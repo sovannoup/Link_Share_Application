ca1f1333d0d48dc899ef46945f68c15deb5eadfd
iMac <imac@iMacs-iMac.local>
2021-12-08 16:30:16 +0700

    db

--- src/Modules/app/config.js
+++ src/Modules/app/config.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://192.168.0.103:3000";
+export const API_URL = "http://192.168.0.105:3000";
 export const IMG_URL = "http://192.168.10.143:3011/imageTemplate/";
 export const IMG_DETIAL_URL = "http://192.168.10.143:3011/imageDetailTemplate/";
 export const BOUGHT_URL = "http://192.168.10.143:3011/templates-bought/1";
